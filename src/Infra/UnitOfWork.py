a6379859d2857c86c36ab51a7abb2007bc526b3b
kossw123 <kossw123@naver.com>
2026-01-31 18:30:30 +0900

    assign ContextManager decorator in UnitOfWork

--- src/Infra/UnitOfWork.py
+++ src/Infra/UnitOfWork.py
@@ -2,8 +2,18 @@
 class UnitOfWork():
   def __init__(self):
     self.seen = set()
+    self.session = None
   def register(self, aggregate):
     self.seen.add(aggregate)
+
+  def __enter(self):
+    return self
+  def __exit__(self, exc_type, exc, tb):
+    if exe_type:
+        self.session.rollback()
+    else:
+        self.session.commit()
+        
   def collect_events(self):
     events = []
     for aggregate in self.seen:
