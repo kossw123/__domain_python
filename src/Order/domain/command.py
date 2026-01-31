0f0485c24ff9ca7fdcc2a772a6308cd64d618c04
kossw123 <kossw123@naver.com>
2026-01-31 18:41:59 +0900

    command.py 업데이트

--- src/Order/domain/command.py
+++ src/Order/domain/command.py
@@ -1,1 +1,22 @@
-asd
+class ICommand():
+    occured_at = datetime.datetime.now()
+class CreateOrder(ICommand):
+    def __init__(self, order_id, customer_id, items):
+        self.order_id = order_id
+        self.customer_id = customer_id
+        self.items = items
+class RequestPayment(ICommand):
+    def __init__(self, order_id):
+        self.order_id = order_id
+class MarkOrderAsPaid(ICommand):
+    def __init__(self, order_id):
+        self.order_id = order_id
+class CancelOrder(ICommand):
+    def __init__(self, order_id):
+        self.order_id = order_id
+class ShipOrder(ICommand):
+    def __init__(self, order_id):
+        self.order_id = order_id
+class CompleteOrder(ICommand):
+    def __init__(self, order_id):
+        self.order_id = order_id
